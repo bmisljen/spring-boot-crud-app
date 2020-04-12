9b249e1d1bc53080ee0f2e47ee6fe1b47d63e913
Boris Misljencevic <bmisljen@gmail.com>
2020-04-12 08:55:50 -0400

    Added JavaScript validation to ensure form fields are not left empty when creating and editing a note

--- src/main/resources/static/js/helpers.js
+++ src/main/resources/static/js/helpers.js
@@ -1,10 +1,11 @@
 
   function validateForm() {
-    var a = document.forms["Form"]["noteName"].value;
-    var b = document.forms["Form"]["noteText"].value;
+    var a = document.forms["noteForm"]["noteName"].value;
+    var b = document.forms["noteForm"]["noteText"].value;
 
     if (a == null || a == "" || b == null || b == "") {
-      alert("Please Fill All Required Field");
+      alert("Note name and note text cannot be empty");
       return false;
     }
+    return true;
   }
